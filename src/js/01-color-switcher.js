77a3dfc1abe9c3433bb98bcc0fa21f4be9401347
Olga-Melnyk <3717129@ukr.net>
2022-12-30 18:31:35 +0200

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+console.dir(startBtn);
 
+let timerId = null;
+stopBtn.disabled = true;
+
+// stopBtn.setAttribute('disabled', true);
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick(evt) {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick(evt) {
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+console.dir(body);
+// body.style.backgroundColor = 'green';
