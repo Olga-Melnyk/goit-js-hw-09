c025ad76badc0768cc7c13f3c33fe2fff26976b6
Olga-Melnyk <3717129@ukr.net>
2022-12-30 23:06:11 +0200

    updating-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,20 +9,28 @@ stopBtn.disabled = true;
 startBtn.addEventListener('click', onStartClick);
 stopBtn.addEventListener('click', onStopClick);
 
-function onStartClick(evt) {
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
+function onStartClick() {
+  isPassive(startBtn);
+  isActive(stopBtn);
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
 
-function onStopClick(evt) {
-  stopBtn.disabled = true;
-  startBtn.disabled = false;
+function onStopClick() {
+  isPassive(stopBtn);
+  isActive(startBtn);
   clearInterval(timerId);
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function isPassive(btnPas) {
+  btnPas.disabled = true;
+}
+
+function isActive(btnAct) {
+  btnAct.disabled = false;
 }
