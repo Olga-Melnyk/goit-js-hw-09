6b8b32b716241c9dc1b7b659b9d390bd3c69b8d8
Olga-Melnyk <3717129@ukr.net>
2023-01-03 16:40:21 +0200

    final fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,6 +4,18 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', onSubmit);
 
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const object = { position, delay };
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(object);
+    } else {
+      reject(object);
+    }
+  });
+}
+
 function onSubmit(e) {
   e.preventDefault();
 
@@ -26,23 +38,3 @@ function onSubmit(e) {
     delay += step;
   }
 }
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  const object = { position, delay };
-  return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve(object);
-    } else {
-      reject(object);
-    }
-  });
-}
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
